74866a5c519c31a206ff73ae1d12882b1649cead
raptor <none@none>
2015-03-08 11:31:32 -0600

    Extract all files not just .cng files

--- cngconverter/cngconverter.py
+++ cngconverter/cngconverter.py
@@ -1,6 +1,6 @@
 '''
-This script will extract all .cng files from .tar files in the
-Complete National Geographic set and decode them into .jpg files.
+This script will extract all files from .tar files in the Complete National
+Geographic set as well decode the .cng files into .jpg files.
 '''
 import fnmatch
 import os
@@ -10,31 +10,39 @@ import tarfile
 
 def extract_decode_tar(tar_file_path, output_path):
 
-    print "Extracting CNG and decoding to JPG: " + tar_file_path
+    print "Extracting files.  Will decode .cng to .jpg: " + tar_file_path
     with tarfile.open(tar_file_path) as tar_file:
         for entry in tar_file:
-            internal_path = entry.path
-            if fnmatch.fnmatch(internal_path, '*.cng'):
-                path_array = internal_path.split(os.sep)
+            fileobj = tar_file.extractfile(entry)
+
+            if not fileobj:
+                continue
+            
+            b = bytearray(fileobj.read())
+            
+            # Handle CNG files differently, we'll convert to JPG
+            if fnmatch.fnmatch(entry.path, '*.cng'):
+                path_array = entry.path.split(os.sep)
                 file_name = os.path.splitext(path_array[-1])[0] + ".jpg"
                 output_file = output_path + os.sep + path_array[-3] + os.sep + path_array[-2] + os.sep + file_name
-                
-                # Now extract and decode the file
-                fileobj = tar_file.extractfile(entry)
-                 
-                b = bytearray(fileobj.read())
-                 
+            
+                # Decoding is super simple
                 for i in range(len(b)):
                     b[i] ^= 0xEF
                 
-                if not os.path.exists(os.path.dirname(output_file)):
-                    os.makedirs(os.path.dirname(output_file))
+            else:
+                output_file = output_path + os.sep + entry.path
                 
-                print output_file
+            # Create intermediate paths
+            if not os.path.exists(os.path.dirname(output_file)):
+                os.makedirs(os.path.dirname(output_file))
                 
-                with open(output_file, "wb") as f:
-                    f.write(b)
-    
+            print output_file
+            
+            # Now write out to file
+            with open(output_file, "wb") as f:
+                f.write(b)
+                
     pass
 
 
@@ -42,7 +50,7 @@ def main():
     if len(sys.argv) < 3:
         print "Usage: " + sys.argv[0] + " INPUT_TAR_FILE OUTPUT_DIR"
         sys.exit(2)
-        
+         
     input_tar = sys.argv[1]
     output_dir = sys.argv[2]
 
